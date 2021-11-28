62c0bcce29d2f3b018b4e5e4fe5c9695d9459564
Vitali Bestolkau <bestolkovv@gmail.com>
2021-11-28 21:53:37 +0100

    Added the major part of the page

--- src/components/UserProfilePage/UserProfilePageComponent.js
+++ src/components/UserProfilePage/UserProfilePageComponent.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import './UserProfilePageComponent.css';
 import {MdLanguage, MdPerson} from "react-icons/md";
-import {Container, Nav, Navbar, NavDropdown, Button, InputGroup, Form} from "react-bootstrap";
+import {Container, Nav, Navbar, NavDropdown, Button, InputGroup, Form, Card, Col, Row} from "react-bootstrap";
 import logo from "../../assets/Logo.svg";
 //import {DashboardBaseComponent} from "../DashboardBase/DashboardBaseComponent";
 
@@ -11,12 +11,22 @@ export function UserProfilePageComponent() {
     const [newPassword, setNewPassword] = useState("");
     const [repeatNewPassword, setRepeatNewPassword] = useState("");
 
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [birthdate, setBirthdate] = useState("");
+    const [address, setAddress] = useState("");
+    const [city, setCity] = useState("");
+    const [postalCode, setPostalCode] = useState("");
+    const [country, setCountry] = useState("");
+
+
     function NavigationBar() {
         const navDropdownTitle = (<MdLanguage color="white" size="2em" />);
 
         return (
             <Navbar bg="dark">
-                <Navbar.Brand href="#home">
+                <Navbar.Brand href="/dashboard">
                     <img
                         src={logo}
                         width="50%"
@@ -40,43 +50,180 @@ export function UserProfilePageComponent() {
         )
     }
 
-    function ChangePasswordField() {
+    function ChangePassword() {
         // const currentPassword = "currentPassword";
 
         return (
             <div id="changePasswordField">
                 <Form>
-                    <h1>Change password</h1>
+                    <h1 className="changePasswordHeader">Change password</h1>
 
-                    <h3>Current password</h3>
-                    <InputGroup style={{width: "60%", margin: "auto"}}>
-                        {/*<InputGroup.Text>Current password</InputGroup.Text>*/}
-                        <Form.Control
-                                      value={currentPassword}
-                                      onChange={(e) => setCurrentPassword(e.target.value)}
-                                      placeholder="******"
-                        />
-                    </InputGroup>
+                    <div className="changePasswordInputField">
+                        <h3>Current password</h3>
+                        <InputGroup className="passwordInputBox" style={{width: "50%", margin: "auto"}}>
+                            {/*<InputGroup.Text>Current password</InputGroup.Text>*/}
+                            <Form.Control
+                                          value={currentPassword}
+                                          onChange={(e) => setCurrentPassword(e.target.value)}
+                                          type="password"
+                                          placeholder="******"
+                                          style={{borderRadius: "30px"}}
+                            />
+                        </InputGroup>
+                    </div>
 
+                    <div className="changePasswordInputField">
                     <h3>New password</h3>
-                    <InputGroup>
-                        {/*<InputGroup.Text>Current password</InputGroup.Text>*/}
-                        <Form.Control className="passwordInputBox"
-                                      value={newPassword}
-                                      onChange={(e) => setNewPassword(e.target.value)}
-                                      placeholder="******" />
-                    </InputGroup>
+                        <InputGroup className="passwordInputBox" style={{width: "50%", margin: "auto"}}>
+                            {/*<InputGroup.Text>Current password</InputGroup.Text>*/}
+                            <Form.Control
+                                          value={newPassword}
+                                          onChange={(e) => setNewPassword(e.target.value)}
+                                          type="password"
+                                          placeholder="******"
+                                          style={{borderRadius: "30px"}}/>
+                        </InputGroup>
+                    </div>
 
-                    <h3>Repeat new password</h3>
-                    <InputGroup>
-                        {/*<InputGroup.Text>Current password</InputGroup.Text>*/}
-                        <Form.Control className="passwordInputBox"
-                                      value={repeatNewPassword}
-                                      onChange={(e) => setRepeatNewPassword(e.target.value)}
-                                      placeholder="******" />
-                    </InputGroup>
+                    <div className="changePasswordInputField">
+                        <h3>Repeat new password</h3>
+                        <InputGroup className="passwordInputBox" style={{width: "50%", margin: "auto"}}>
+                            {/*<InputGroup.Text>Current password</InputGroup.Text>*/}
+                            <Form.Control
+                                          value={repeatNewPassword}
+                                          onChange={(e) => setRepeatNewPassword(e.target.value)}
+                                          type="password"
+                                          placeholder="******"
+                                          style={{borderRadius: "30px"}}/>
+                        </InputGroup>
+                    </div>
+                    <Button onClick={() => ChangePassword()}
+                            style={{borderRadius: "30px", backgroundColor: "#EFA9AE", color: "#02021E", margin: "80px 0px 0px 0px", fontSize: "22px", border: "transparent"}}>Change password</Button>
                 </Form>
             </div>
+        )
+    }
+
+    function EditPersonalInfo() {
+
+        return (
+            <Card id="editPersonalInfoField" style={{ backgroundColor: "#ff000000"}}>
+                <Form>
+                    <div className="halfOfPersonalInfoField">
+                        <h1>Personal details</h1>
+                        <Row>
+                            <Col xs lg="7">
+                                <Form.Label>First name</Form.Label>
+                                <Form.Control readOnly value="First name hkjlk;l" />
+                            </Col>
+                            <Col xs lg="5">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control readOnly value="Email @uygknlk" />
+                            </Col>
+                        </Row>
+                        {/*<div className="leftQuarterOfPersonalInfoField">*/}
+                        {/*    <div className="editPersonalInfoInputSingleField">*/}
+                        {/*        <h4 className="personalInfoInputFieldHeader">First name</h4>*/}
+                        {/*        <InputGroup style={{width: "60%"}}>*/}
+                        {/*            <Form.Control value={firstName}*/}
+                        {/*                          onChange={(e) => setFirstName(e.target.value)}*/}
+                        {/*                          type="text"*/}
+                        {/*                          placeholder="First name"*/}
+                        {/*                          aria-required="true"*/}
+                        {/*                          style={{borderRadius: "30px"}}/>*/}
+                        {/*        </InputGroup>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="editPersonalInfoInputSingleField">*/}
+                        {/*        <h4 className="personalInfoInputFieldHeader">Last name</h4>*/}
+                        {/*        <InputGroup>*/}
+                        {/*            <Form.Control value={lastName}*/}
+                        {/*                          onChange={(e) => setLastName(e.target.value)}*/}
+                        {/*                          type="text"*/}
+                        {/*                          placeholder="Last name"*/}
+                        {/*                          aria-required="true"*/}
+                        {/*                          style={{borderRadius: "30px"}}/>*/}
+                        {/*        </InputGroup>*/}
+                        {/*    </div>*/}
+                        {/*</div>*/}
+                        <div className="rightQuarterOfPersonalInfoField">
+                            <div className="editPersonalInfoInputSingleField">
+                                <h4 className="personalInfoInputFieldHeader">Email</h4>
+                                <InputGroup>
+                                    <Form.Control value={email}
+                                                  onChange={(e) => setEmail(e.target.value)}
+                                                  type="email"
+                                                  placeholder="Email"
+                                                  aria-required="true"
+                                                  style={{borderRadius: "30px"}}/>
+                                </InputGroup>
+                            </div>
+                            <div className="editPersonalInfoInputSingleField">
+                                <h4 className="personalInfoInputFieldHeader">Date of birth</h4>
+                                <InputGroup>
+                                    <Form.Control value={birthdate}
+                                                  onChange={(e) => setBirthdate(e.target.value)}
+                                                  type="date"
+                                                  placeholder="dd-mm-yyyy"
+
+                                                  style={{borderRadius: "30px"}}/>
+                                </InputGroup>
+                            </div>
+                        </div>
+                    </div>
+                    {/*<div id="divideLine"/>*/}
+                    <div className="halfOfPersonalInfoField">
+                        <h1>Location details</h1>
+                        <div className="leftQuarterOfPersonalInfoField">
+                            <div className="editPersonalInfoInputSingleField">
+                                <h4 className="personalInfoInputFieldHeader">Address</h4>
+                                <InputGroup>
+                                    <Form.Control value={address}
+                                                  onChange={(e) => setAddress(e.target.value)}
+                                                  type="text"
+                                                  placeholder="Address"
+                                                  aria-required="true"
+                                                  style={{borderRadius: "30px"}}/>
+                                </InputGroup>
+                            </div>
+                            <div className="editPersonalInfoInputSingleField">
+                                <h4 className="personalInfoInputFieldHeader">City</h4>
+                                <InputGroup>
+                                    <Form.Control value={city}
+                                                  onChange={(e) => setCity(e.target.value)}
+                                                  type="text"
+                                                  placeholder="City"
+                                                  aria-required="true"
+                                                  style={{borderRadius: "30px"}}/>
+                                </InputGroup>
+                            </div>
+                        </div>
+                        <div className="rightQuarterOfPersonalInfoField">
+                            <div className="editPersonalInfoInputSingleField">
+                                <h4 className="personalInfoInputFieldHeader">Post code</h4>
+                                <InputGroup>
+                                    <Form.Control value={postalCode}
+                                                  onChange={(e) => setPostalCode(e.target.value)}
+                                                  type="text"
+                                                  placeholder="1234 AB"
+                                                  aria-required="true"
+                                                  style={{borderRadius: "30px"}}/>
+                                </InputGroup>
+                            </div>
+                            <div className="editPersonalInfoInputSingleField">
+                                <h4 className="personalInfoInputFieldHeader">Country</h4>
+                                <InputGroup>
+                                    <Form.Control value={country}
+                                                  onChange={(e) => setCountry(e.target.value)}
+                                                  type="text"
+                                                  placeholder="Country"
+                                                  aria-required="true"
+                                                  style={{borderRadius: "30px"}}/>
+                                </InputGroup>
+                            </div>
+                        </div>
+                    </div>
+                </Form>
+            </Card>
         )
     }
 
@@ -85,7 +232,8 @@ export function UserProfilePageComponent() {
             <div className="Full">
                 <NavigationBar/>
                 <div id="mainProfileField">
-                    <ChangePasswordField/>
+                    <ChangePassword/>
+                    <EditPersonalInfo/>
                 </div>
             </div>
         </>
--- src/components/router.js
+++ src/components/router.js
@@ -1,4 +1,53 @@
-import React, { useEffect } from 'react';
+// import React, { useEffect } from 'react';
+// import { Route, Switch, useHistory, useLocation, Redirect } from 'react-router-dom';
+//
+// import * as Components from './index';
+// import ProtectedRoute from './ProtectedRoute';
+// import { useAuth } from '../actions';
+// import { Spinner } from 'react-bootstrap';
+// import { authAtom } from '../state';
+// import { useRecoilValue } from 'recoil';
+//
+//
+// const Routes = () => {
+//     const authActions = useAuth()
+//     const authState = useRecoilValue(authAtom)
+//
+//     useEffect(() => {
+//         authActions.refreshToken()
+//     }, [])
+//
+//
+//     return (
+//         <main>
+//             <Switch>
+//                 {/*<ProtectedRoute path='/profile'>*/}
+//                 {/*    <Components.UserProfilePageComponent />*/}
+//                 {/*</ProtectedRoute>*/}
+//                 {/*<ProtectedRoute path='/dashboard'>*/}
+//                 {/*    <Components.DashboardBaseComponent />*/}
+//                 {/*</ProtectedRoute>*/}
+//                 <ProtectedRoute path='/dashboard'>
+//                     <Components.UserProfilePageComponent />
+//                 </ProtectedRoute>
+//                 <ProtectedRoute path='/users'>
+//                     <Components.UsersTableComponent />
+//                 </ProtectedRoute>
+//
+//                 {authState ?
+//                     // <Route path='*' component={Components.DashboardBaseComponent} />
+//                     <Route path='*' component={Components.UserProfilePageComponent} />
+//                     :
+//                     <Route path='*' component={Components.LrBaseComponent} />
+//                 }
+//             </Switch>
+//         </main>
+//     )
+// }
+//
+// export default Routes
+
+import React, { useEffect, useState } from 'react';
 import { Route, Switch, useHistory, useLocation, Redirect } from 'react-router-dom';
 
 import * as Components from './index';
@@ -11,35 +60,34 @@ import { useRecoilValue } from 'recoil';
 
 const Routes = () => {
     const authActions = useAuth()
-    const authState = useRecoilValue(authAtom)
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
-        authActions.refreshToken()
+        authActions.refreshToken().then(x => setIsLoading(false))
     }, [])
+
+    if (isLoading) {
+        return (
+            <div style={{justifyContent: "center", textAlign: "center"}}>
+                <Spinner animation="border" style={{ fontSize: 500 }} />
+            </div>
+        )
+    }
 
 
     return (
         <main>
             <Switch>
-                {/*<ProtectedRoute path='/profile'>*/}
-                {/*    <Components.UserProfilePageComponent />*/}
-                {/*</ProtectedRoute>*/}
-                {/*<ProtectedRoute path='/dashboard'>*/}
-                {/*    <Components.DashboardBaseComponent />*/}
-                {/*</ProtectedRoute>*/}
-                <ProtectedRoute path='/dashboard'>
+                <ProtectedRoute path='/profile'>
                     <Components.UserProfilePageComponent />
+                </ProtectedRoute>
+                <ProtectedRoute path='/dashboard'>
+                    <Components.DashboardBaseComponent />
                 </ProtectedRoute>
                 <ProtectedRoute path='/users'>
                     <Components.UsersTableComponent />
                 </ProtectedRoute>
-
-                {authState ?
-                    // <Route path='*' component={Components.DashboardBaseComponent} />
-                    <Route path='*' component={Components.UserProfilePageComponent} />
-                    :
-                    <Route path='*' component={Components.LrBaseComponent} />
-                }
+                <Route path={['/login', '/']} component={Components.LrBaseComponent} />
             </Switch>
         </main>
     )
