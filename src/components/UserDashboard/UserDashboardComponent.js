644518248a33f6fa580bf1e9797a5dda27db9f6b
Brandon Lodowica <brandonlodowica@hotmail.com>
2021-12-14 16:58:07 +0100

    UserDashboard extra Functionality added

--- src/components/UserDashboard/UserDashboardComponent.js
+++ src/components/UserDashboard/UserDashboardComponent.js
@@ -1,12 +1,10 @@
 import './userDashboardComponent.css'
 import React from 'react';
-import { Tabs, Row, Col, Container, Tab, Card, Navbar, Nav, NavDropdown, Glyphicon, Button, Table, Badge } from 'react-bootstrap';
-import { atom, useRecoilState, useRecoilValue } from 'recoil';
+import { Row, Col, Container, Card, Button, Table, Badge, Modal, Form } from 'react-bootstrap';
+import { useRecoilValue } from 'recoil';
 import { useEffect, useState } from 'react';
 import { useRequestWrapper } from '../../middleware';
-import logo from '../../assets/Logo.svg';
-import { MdLanguage, MdPerson } from "react-icons/md";
-
+import { MdOutlineError, MdContentCopy, MdLibraryAddCheck } from "react-icons/md";
 import i18n from "i18next";
 import { useTranslation, initReactI18next } from "react-i18next";
 import languageDetector from 'i18next-browser-languagedetector';
@@ -14,48 +12,26 @@ import HttpApi from 'i18next-http-backend';
 import i18next from 'i18next';
 
 import { authAtom } from '../../state';
-import {NavigationBarComponent} from '../';
-
-import { Localization } from '../Localization/LocalizationComponent'
+import { NavigationBarComponent } from '../';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { Localization } from '../Localization/LocalizationComponent';
 <Localization />
-i18n
-    .use(initReactI18next)
-    .use(languageDetector)
-    .use(HttpApi)
-
-    .init({
-        //if you're using a language detector don't define the lng option
-        fallbackLng: "en",
-
-        //language detection
-        detection: {
-            order: ['cookie', 'htmlTag', 'localStorage', 'path', 'subdomain'],
-            caches: ['cookie'],
-        },
-
-        //i18next http backend
-        backend: {
-            loadPath: '../../assets/locales/{{lng}}/translation.json'
-
-        },
-
-        react: { useSuspense: false }
-    })
-
-
 
 
 export function UserDashboard() {
 
-    const userState = useRecoilValue(authAtom);
 
-    const navDropdownTitle = (<MdLanguage color="white" size="2em" />);
+    const userState = useRecoilValue(authAtom);
+    const [copiedText, setCopiedText] = useState("");
+    const [modalShow, setModalShow] = useState(false);
+    const [detailedLicense, setDetailedLicense] = useState();
+    // const navDropdownTitle = (<MdLanguage color="white" size="2em" />);
     const [licenses, setLicenses] = useState(null);
     const requestWrapper = useRequestWrapper()
     const baseUrl = `${process.env.REACT_APP_BACKEND_API_URL}/api/`;
     const myCurrentTime = new Date();
     const { t } = useTranslation();
-
+    const [updateModal, setUpdateModal] = useState(false);
 
     useEffect(() => {
         if (licenses == null) {
@@ -67,7 +43,6 @@ export function UserDashboard() {
                     // var mm = d.getMonth() + 1;
                     // var yy = d.getFullYear();
                     // response.expirationDate = dd + "/" + mm + "/" + yy;
-                    console.log(response)
                     setLicenses(response)
 
                 })
@@ -81,9 +56,119 @@ export function UserDashboard() {
     })
 
 
+    function LicenseModal({ onHide, license, show }) {
+        const [detailedData, setDetailedData] = useState();
+
+        //get the needed data
+        useEffect(() => {
+            requestWrapper.get(`${baseUrl}license/${license.id}`)
+                .then(response => {
+                    if (response.expiresAt) {
+                        var displayDate = new Date(Date.parse(response.expiresAt))
+                        response.expiresAt = `${displayDate.getDate() + 1}-${displayDate.getMonth() + 1}-${displayDate.getFullYear() + 1}`
+                    }
+                    setDetailedData(response);
+                }).catch((er) => {
+                    setLicenses(null)
+                    console.log(er)
+                });
+        }, [license, updateModal])
+
+        if (!detailedData) return null;
+
+        //detailed license modal
+        return (
+            <Modal
+                show={show}
+                onHide={onHide}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header closeButton className="DetailedRecordModalHeader">
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        License: <b>{detailedData.licenseKey}</b>
+                        <div className="d-inline-flex ms-1">
+                            <CopyToClipboard text={license.licenseKey} onCopy={() => setCopiedText(license.licenseKey)}>
+                                {copiedText == license.licenseKey ?
+                                    <MdLibraryAddCheck style={{ color: "#4c4e50" }} className="PointOnHover" />
+                                    :
+                                    <MdContentCopy style={{ color: "#7d93af" }} className="PointOnHover" />
+                                }
+                            </CopyToClipboard>
+                        </div>
+                        <div className="d-inline-flex ms-3">
+                            {detailedData.active ?
+                                <Button className="ps-1 pe-1 pt-0 pb-0 NotClickable" variant="success" size="lg">{t('dashboard_active')}</Button>
+                                :
+                                <>
+                                    <Button className="ps-1 pe-1 pt-0 pb-0 NotClickable" variant="danger" size="lg">{t('dashboard_inactive')}</Button>
+                                </>}
+                        </div>
+                    </Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                    <Row>
+                        <Col xs lg="7">
+                            <Form.Label>{t('dashboard_product')}</Form.Label>
+                            <Form.Control readOnly value={detailedData.productName} />
+                        </Col>
+                        <Col xs lg="5">
+                            <Form.Label>{t('dashboard_reccurence')}</Form.Label>
+                            <Form.Control readOnly value={detailedData.recurrence} />
+                        </Col>
+                    </Row>
+
+                    <Row className="mt-2">
+                        <Col xs lg="7">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control readOnly value={detailedData.email} />
+                        </Col>
+                        <Col xs lg="5">
+                            <Form.Label>{t('dashboard_purchase_location')}</Form.Label>
+                            <Form.Control readOnly value={detailedData.purchaseLocation} />
+                        </Col>
+                    </Row>
+
+                    <Row className="mt-2">
+                        <Col xs lg="3">
+                            <Form.Label>{t('dashboard_activations')}</Form.Label>
+                            <Form.Control readOnly value={`${detailedData.activations}/${detailedData.maxUses}`} />
+                            {detailedData.activations > detailedData.maxUses &&
+                                <MdOutlineError color="red" size="1.5em" className="d-flex ms-auto DetailedUserDanger" />
+                            }
+                        </Col>
+                        <Col xs lg="4">
+                            <Form.Label>{t('dashboard_expirationdate')}</Form.Label>
+                            <Form.Control readOnly value={detailedData.expiresAt ? `${detailedData.expiresAt}` : "-"} />
+                        </Col>
+                        <Col xs lg="5">
+                            <Form.Label>{t('dashboard_deactivated_reason')}</Form.Label>
+                            <Form.Control readOnly value={detailedData.endedReason ? `${detailedData.endedReason}` : "-"} />
+                        </Col>
+                    </Row>
+
+
+
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button className={"btn-" + (detailedData.active ? 'danger' : 'primary')} onClick={() => {
+                        console.log('redirected');
+                        window.location.href = 'https://techtycoons.gumroad.com/?recommended_by=library'
+                    }
+                    }>
+                        {detailedData.active ? <>{t('dashboard_disable')}</> : <>{t('dashboard_enable')}</>}
+                    </Button>
+                </Modal.Footer>
+            </Modal >
+        );
+    }
+
+
     return (
         <div className="Full" >
-            <NavigationBarComponent/>
+            <NavigationBarComponent />
             <Container fluid className="pt-3">
                 <Row>
                     <Col>
@@ -99,13 +184,15 @@ export function UserDashboard() {
                                 <Table striped hover responsive>
                                     <thead>
                                         <tr>
+
                                             <th scope="col">{t('dashboard_licenses')}</th>
+                                            <th scope='col' style={{ marginLeft: 10 }}>{t('dashboard_licensekey')}</th>
                                             <th scope="col">{t('dashboard_uses')}</th>
                                             <th scope="col">{t('dashboard_tier')}</th>
                                             <th scope="col">{t('dashboard_payment_period')}</th>
                                             <th scope="col">{t('dashboard_expirationdate')}</th>
                                             <th scope="col">{t('dashboard_status')}</th>
-
+                                            <th></th>
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -115,12 +202,28 @@ export function UserDashboard() {
                                             return (
 
                                                 <tr key={license.id}>
-                                                    <td className="align-middle" style={{ width: "100px" }}>{license.productName}</td>
-                                                    <td className="align-middle"> 1 / {license.maxUses}</td>
-                                                    <td className="align-middle">{license.tier}</td>
-                                                    <td className="align-middle">{license.reaccurence}</td>
-                                                    <td className="align-middle">{license.expirationDate != null ? license.expirationDate.split('T')[0] : <p className="align-middle">-</p>}</td>
-                                                    <td className="align-middle">{myCurrentTime >= license.expirationDate ? <Badge bg="success">{t('dashboard_active')}</Badge> : <Badge bg="danger">{t('dashboard_inactive')}</Badge>}</td>
+
+                                                    <td className="align-middle userDashboardLicenseName" style={{ width: "150px" }}>{license.productName}</td>
+                                                    <td className="align-middle Userdashboardlicensekey" style={{ width: "350px" }}>
+
+                                                        {license.licenseKey}
+                                                        <CopyToClipboard text={license.licenseKey}
+                                                            onCopy={() => setCopiedText(license.licenseKey)}>
+                                                            {copiedText == license.licenseKey ?
+                                                                <MdLibraryAddCheck style={{ color: "#4c4e50" }} className="ms-2 PointOnHover" />
+                                                                :
+                                                                <MdContentCopy style={{ color: "#7d93af" }} className="ms-2 PointOnHover" />
+                                                            }
+                                                        </CopyToClipboard>
+                                                    </td >
+                                                    <td className="align-middle Userdashboardsmalltext"> {license.activation} / {license.maxUses}</td>
+                                                    <td className="align-middle Userdashboardsmalltext">{license.tier}</td>
+                                                    <td className="align-middle Userdashboardsmalltext">{license.reaccurence}</td>
+                                                    <td className="align-middle Userdashboardsmalltext">{license.expirationDate != null ? license.expirationDate.split('T')[0] : <p className="align-middle">-</p>}</td>
+                                                    <td className="align-middle ">{myCurrentTime >= license.expirationDate ? <Badge bg="success">{t('dashboard_active')}</Badge> : <Badge bg="danger">{t('dashboard_inactive')}</Badge>}</td>
+                                                    <td className="align-middle" style={{ width: "110px" }}>
+                                                        <Button className="p-1" onClick={() => { setModalShow(true); setDetailedLicense(license) }}>{t('dashboard_more')}</Button>
+                                                    </td>
 
 
 
@@ -128,6 +231,11 @@ export function UserDashboard() {
                                             )
                                         }
                                         ) : null}
+                                        {(detailedLicense && modalShow) && <LicenseModal
+                                            onHide={() => setModalShow(false)}
+                                            license={detailedLicense}
+                                            show={modalShow}
+                                        />}
                                     </tbody>
                                 </Table>
 
