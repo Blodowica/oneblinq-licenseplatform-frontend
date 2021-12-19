6390220bdff1fa8d60e4475b74e6c69d8acc66f4
ObviousSloth <j.hooftman2k@gmail.com>
2021-12-19 15:45:48 +0100

    Addded and installed cypress, test files
    Installed cypress and added it to project, added basic test files with a test template

---
+++ cypress/integration/OneBlinqTests/AccessTokens.test.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+describe('Test description', ()=>{
+    it('test title', ()=>{
+        cy.visit('http://localhost:3000') // visits the local host, backend and front end needs to be running
+      
+    })
+})
---
+++ cypress/integration/OneBlinqTests/hello-world.test.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+describe('ahaha', ()=>{
+    it('test one', ()=>{
+        cy.visit('https://www.google.com/')
+      
+    })
+})
