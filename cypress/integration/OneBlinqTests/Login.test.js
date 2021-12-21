8a04c8b4daf544d9fcf93921ec28149442faaf00
ObviousSloth <j.hooftman2k@gmail.com>
2021-12-21 12:45:39 +0100

    added test  for logging in
    Added data-test tags in login component and created login test

--- cypress/integration/OneBlinqTests/Login.test.js
+++ cypress/integration/OneBlinqTests/Login.test.js
@@ -6,3 +6,18 @@ describe('Test description', ()=>{
       
     })
 })
+
+describe('Logging in', ()=>{
+    it('Login test', ()=>{
+        const url = Cypress.env('REACT_APP_FRONTED_URL')
+        cy.log(url)
+        cy.viewport(1920,1080)
+        cy.visit(url)
+     
+        cy.get('[data-testId=LoginEmail]').type('thomasvandermolen2@gmail.com')
+        cy.get('[data-testId=LoginPassword]').type('admin')
+        cy.get('[data-testId=LoginButton]').click()
+
+      
+    })
+})
--- cypress/integration/OneBlinqTests/hello-world.test.js
+++ cypress/integration/OneBlinqTests/hello-world.test.js
@@ -1,8 +1,17 @@
 /// <reference types="cypress" />
 
+
 describe('ahaha', ()=>{
     it('test one', ()=>{
-        cy.visit('https://www.google.com/')
+        const url = Cypress.env('REACT_APP_FRONTED_URL')
+        cy.log(url)
+        cy.viewport(1920,1080)
+        cy.visit(url)
+     
+        cy.get('[data-testId=LoginEmail]').type('thomasvandermolen2@gmail.com')
+        cy.get('[data-testId=LoginPassword]').type('admin')
+        cy.get('[data-testId=LoginButton]').click()
+
       
     })
 })
