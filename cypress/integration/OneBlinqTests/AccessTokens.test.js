d3ec1aeced435573f906d4de7f25e21087d0d7b0
Vitali Bestolkau <bestolkovv@gmail.com>
2022-01-20 14:33:41 +0100

    Corrected Access Token part

--- cypress/integration/OneBlinqTests/AccessTokens.test.js
+++ cypress/integration/OneBlinqTests/AccessTokens.test.js
@@ -5,17 +5,6 @@
  // it is best to add a command in the commands.js file(support folder) and call it in tests
 describe('Test description', ()=>{
     it('test title', ()=>{
-        /* ==== Generated with Cypress Studio ==== */
-        cy.visit('ttlicensing.netlify.app');
-        cy.get('[data-testid="LoginEmail"]').clear();
-        cy.get('[data-testid="LoginEmail"]').type('admin@gmail.com');
-        cy.get('[data-testid="LoginPassword"]').clear();
-        cy.get('[data-testid="LoginPassword"]').type('admin');
-        cy.get('[data-testid="LoginButton"]').click();
-        cy.get('#basic-nav-dropdown > svg').click();
-        cy.get('[href="/profile"]').click();
-        cy.get('.col > .d-flex').click();
-        cy.get('.justify-content-between > :nth-child(1) > .btn').click();
-        /* ==== End Cypress Studio ==== */
+
     })
 })
